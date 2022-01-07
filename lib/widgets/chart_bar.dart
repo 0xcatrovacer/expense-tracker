304e048c940fd4080185b869722c9b5f996568aa
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-07 21:40:16 +0530

    populated chart bar with values

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -30,7 +30,7 @@ class ChartBar extends StatelessWidget {
                 heightFactor: spendingPctg,
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    color: Theme.of(context).colorScheme.primary,
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
