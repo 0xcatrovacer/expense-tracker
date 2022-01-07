46468429ed24c5d1d96e2331d6dcc91f072c51da
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-07 21:13:44 +0530

    added data to chart widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -4,20 +4,20 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  final List<Transaction> recentTransactions;
+  final List<Transaction> recentUserTransactions;
 
-  Chart(this.recentTransactions);
+  Chart(this.recentUserTransactions);
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
 
       var totalSum = 0.0;
-      for (var i = 0; i < recentTransactions.length; i++) {
-        if (recentTransactions[i].date?.day == weekDay.day &&
-            recentTransactions[i].date?.month == weekDay.month &&
-            recentTransactions[i].date?.year == weekDay.year) {
-          totalSum += recentTransactions[i].amount as num;
+      for (var i = 0; i < recentUserTransactions.length; i++) {
+        if (recentUserTransactions[i].date?.day == weekDay.day &&
+            recentUserTransactions[i].date?.month == weekDay.month &&
+            recentUserTransactions[i].date?.year == weekDay.year) {
+          totalSum += recentUserTransactions[i].amount as num;
         }
       }
 
